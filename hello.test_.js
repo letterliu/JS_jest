3ddb0991ee8cce0c483e106354d162007e4a8f8c
letterliu <letter0206@gmail.com>
2019-08-27 20:58:40 +0800

    modify js_08.js

--- hello.test_.js
+++
@@ -1,7 +0,0 @@
-// hello.test.js
-let hello = require('hello.js')
-
-test('should get "Hello world"', () => {
-  expect(hello()).toBe('Hello world') // 测试成功
-  // expect(hello()).toBe('Hello') // 测试失败
-})

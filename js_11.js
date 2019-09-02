ebb384e83f99347359e2a2fca4e882c8c3ee528d
letterliu <letter0206@gmail.com>
2019-09-02 14:48:43 +0800

    modify js_11.js

--- js_11.js
+++ js_11.js
@@ -5,47 +5,65 @@
 並利用do while計算，印出總費用。
 */
 
-const periods = process.argv[2];
-// console.log(periods);
+// periods
+const result = gymnasium(process.argv[2]);
+console.info(result);
 
-let membershipDues = 500;
-let cashDiscount = 79 / 100;
-let bonusPoint = 200;
 
-let i = 1;
-let sum = 0;
-
-do {
-  if (i === 1) {
-    sum = membershipDues * cashDiscount;
-    console.log('新會員獨享禮 79 折');
-  } else {
-    sum += membershipDues;
+function gymnasium(receivedata) {
+  // 主程式，例外時拋出錯誤資訊
+  try {
+    const validData = formatData(receivedata);
+    const membershipDues = members(validData);
+    return announced(membershipDues);
   }
-  if (!(i % 5)) {
-    sum -= bonusPoint;
-    console.log('每滿五期，會員折扣金 200 元');
+  catch (e) {
+    return e.message;
   }
-  i++;
-} while (i <= periods);
+  finally {
+    console.info(`Ψ(｀∀´#)ﾉ 好想健身房新開幕，新會員優惠中。`);
+  }
 
-console.log(`報名總期數：${periods} 期，總金額：${sum} 元`);
+  // 正則表達式 資料瞥除有效正整數以外所有數值 
+  function formatData(data) {
+    const ELIMINATEDATA = /[^0-9]|^0$/g;
+    const invalidData = ELIMINATEDATA.test(data);
+    if (invalidData || !data.trim()) {
+      throw Error(`[${data}] 格式錯誤，請重新輸入有效的正整數。`);
+    }
+    const validData = Number(data);
+    return validData;
+  }
 
+  // 會員費用總計算
+  function members(periods) {
+    const membershipDues = 500, cashDiscount = 79 / 100, bonusPoint = 200;
+    let i = 1, sum = 0, cumulativeBonus = 0;
+    cumulativeBonus = parseInt(periods / 5) * bonusPoint;
 
+    do {
+      if (i === 1) {
+        sum = membershipDues * cashDiscount;
+      } else {
+        sum += membershipDues;
+      }
+      if (!(i % 5)) {
+        sum -= bonusPoint;
+      }
+    } while (++i <= periods);
 
+    return {
+      periods: periods,
+      cumulativeBonus: cumulativeBonus,
+      sum: sum
+    };
+  }
 
-
-// function test(periods, result) {
-//   if (periods) {
-//     result = memberDues * discount;
-//   }
-//   for (let i = 2; i <= periods; i++) {
-//     if (!(i % 5)) {
-//       result = result - bonus;
-//     }
-//     result = result + memberDues;
-//   }
-//   console.log(result);
-
-// }
-// test(periods);
+  // 宣告總期數與總會費
+  function announced(membershipDues) {
+    console.info('新會員獨享禮 79 折。');
+    console.info(`會員累積折扣金 ${membershipDues.cumulativeBonus} 元。`);
+    console.info(`報名總期數：${membershipDues.periods} 期，總金額：${membershipDues.sum} 元。`);
+    return membershipDues;
+  }
+}

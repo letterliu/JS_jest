fa22158dda763f4c08c4df3f0e50b3a3b418e1f2
letterliu <letter0206@gmail.com>
2019-09-09 11:11:13 +0800

    modify jest js_06.test.js

--- jest/js_05.test.js
+++ jest/js_05.test.js
@@ -21,65 +21,48 @@ describe('遞迴主程式：recursionGame', () => {
       const result = Matchers.recursionGame(integer);
       expect(result).toContain(`輸入值[${integer}] => 請重新輸入大於或等於 4 以上偶數值。`);
     });
+  });
 
-    describe('空值與資料格式過濾', () => {
-      it('過濾：輸入值[ ] => 格式錯誤，請重新輸入有效的正整數。', () => {
-        const invalidData = ' ';
-        const result = () => Matchers.formatData(invalidData);
-        expect(result).toThrow(`輸入值[${invalidData}] => 格式錯誤，請重新輸入有效的正整數。`);
-      });
-      it('過濾：輸入有效數值，字串轉成數值。', () => {
-        const validData = '6';
-        const result = Matchers.formatData(validData);
-        expect(result).toBe(6);
-      });
+  describe('空值與資料格式過濾', () => {
+    it('過濾：輸入值[ ] => 格式錯誤，請重新輸入有效的正整數。', () => {
+      const invalidData = ' ';
+      const result = () => Matchers.formatData(invalidData);
+      expect(result).toThrow(`輸入值[${invalidData}] => 格式錯誤，請重新輸入有效的正整數。`);
     });
-
-    describe('判斷4以上(包含4)並為偶數正整數', () => {
-      it('過濾：輸入值[2] => 請重新輸入大於或等於 4 以上偶數值。', () => {
-        const integer = 2;
-        const result = () => Matchers.evenNumber(integer);
-        expect(result).toThrow(`輸入值[${integer}] => 請重新輸入大於或等於 4 以上偶數值。`);
-      });
-      it('過濾：保留有效數值。', () => {
-        const integer = 6;
-        const result = Matchers.evenNumber(integer);
-        expect(result).toBe(6);
-      });
-    });
-
-    describe('遞迴運算器(數值：6)', () => {
-      it('總和: 32。', () => {
-        const integer = 6;
-        const result = Matchers.recursionSum(integer);
-        expect(result).toBe(32);
-      });
-    });
-
-    describe('統計遞迴結果', () => {
-      it('打印：遞迴函數總和: 32。', () => {
-        const sum = 32;
-        const result = Matchers.statistic(sum);
-        expect(result).toContain(`遞迴函數總和：${sum}`);
-      });
+    it('過濾：輸入有效數值，字串轉成數值。', () => {
+      const validData = '6';
+      const result = Matchers.formatData(validData);
+      expect(result).toBe(6);
     });
   });
 
+  describe('判斷4以上(包含4)並為偶數正整數', () => {
+    it('過濾：輸入值[2] => 請重新輸入大於或等於 4 以上偶數值。', () => {
+      const integer = 2;
+      const result = () => Matchers.evenNumber(integer);
+      expect(result).toThrow(`輸入值[${integer}] => 請重新輸入大於或等於 4 以上偶數值。`);
+    });
+    it('過濾：保留有效數值。', () => {
+      const integer = 6;
+      const result = Matchers.evenNumber(integer);
+      expect(result).toBe(6);
+    });
+  });
 
+  describe('遞迴運算器(數值：6)', () => {
+    it('總和: 32。', () => {
+      const integer = 6;
+      const result = Matchers.recursionSum(integer);
+      expect(result).toBe(32);
+    });
+  });
 
-  // describe('發散級數運轉', () => {
-  //   it('總和: 8。', () => {
-  //     const integer = 12;
-  //     const result = Matchers.seriesSum(integer);
-  //     expect(result).toBe(8);
-  //   });
-  // });
+  describe('統計遞迴結果', () => {
+    it('打印：遞迴函數總和: 32。', () => {
+      const sum = 32;
+      const result = Matchers.statistic(sum);
+      expect(result).toContain(`遞迴函數總和：${sum}`);
+    });
+  });
 
-  // describe('統計級數結果', () => {
-  //   it('打印：發散級數總和: 8。', () => {
-  //     const sum = 8;
-  //     const result = Matchers.statistic(sum);
-  //     expect(result).toContain(`${result}`);
-  //   });
-  // });
-})
+});
--- js_06.js
+++ js_06.js
@@ -3,6 +3,16 @@
 輸出第一個和第三個名字的單數個字母。
 */
 
+// Jest
+module.exports = {
+  alphabetGame,
+  formatData,
+  attendance,
+  selection,
+  oddCharacters,
+  announced
+};
+
 class FormatError extends Error {
   constructor(msg) {
     super(msg);
@@ -29,44 +39,48 @@ function alphabetGame(receivedata) {
   finally {
     console.info('(❛◡❛✿) 好想認識字母表，從挑戰名字開始 !! Fighting !!');
   }
-
-  // 判斷格式是否全英文字母，不分大小寫
-  function formatData(data) {
-    const ELIMINATEDATA = /[^a-z]/i;
-    const invalidData = data.map(value => ELIMINATEDATA.test(value)).some(value => value);
-    const validNull = data.includes('');
-    if (invalidData || validNull) {
-      throw new FormatError(`輸入值[${data}] => 格式錯誤，請重新輸入有效英文字母。`);
-    }
-    return data;
-  }
-
-  // 參與會員需要四位
-  function attendance(members) {
-    const group = 4;
-    if (members.length !== group) {
-      throw Error(`遊戲人數 ${members.length} 人 => 尚未達標，請重新輸入參與挑戰，共四位英文名字。`);
-    }
-    return members;
-  }
-
-  // // 判斷符合身份的黃金會員
-  function selection(members) {
-    const goldMembers = members.filter((list, index) => !(index % 2));
-    console.info(`符合挑選資格：${goldMembers[0]} 和 ${goldMembers[1]}。`);
-    return goldMembers;
-  }
-
-  // // 黃金會員名字拆解取出奇數字母
-  function oddCharacters(name) {
-    const characters = name.map(letters => letters.split('').filter((letters, index) => ++index % 2));
-    return characters.reduce((previous, next) => [...previous, ...next]);
-  }
-
-  function announced(answer) {
-    return `隨機取樣新字母：[${answer}]，您都唸對了嗎？`;
-  }
 }
+
+// 判斷格式是否全英文字母，不分大小寫
+function formatData(data) {
+  const ELIMINATEDATA = /[^a-z]/i;
+  const invalidData = data.map(value => ELIMINATEDATA.test(value)).some(value => value);
+  const validNull = data.includes('');
+  if (invalidData || validNull) {
+    throw new FormatError(`輸入值[${data}] => 格式錯誤，請重新輸入有效英文字母。`);
+  }
+  return data;
+}
+
+// 參與會員需要四位
+function attendance(members) {
+  const group = 4;
+  if (members.length !== group) {
+    throw Error(`遊戲人數 ${members.length} 人 => 尚未達標，請重新輸入參與挑戰，共四位英文名字。`);
+  }
+  return members;
+}
+
+// 判斷符合身份的黃金會員
+function selection(members) {
+  const goldMembers = members.filter((list, index) => !(index % 2));
+  console.info(`符合挑選資格：${goldMembers[0]} 和 ${goldMembers[1]}。`);
+  return goldMembers;
+}
+
+// 黃金會員名字拆解取出奇數字母
+function oddCharacters(name) {
+  const characters = name.map(letters => letters.split('').filter((letters, index) => ++index % 2));
+  return characters.reduce((previous, next) => [...previous, ...next]);
+}
+
+// 執行打印結果
+function announced(answer) {
+  return `隨機取樣新字母：[${answer}]，您都唸對了嗎？`;
+}
+
+
+//-----------------------------------------------------------------------------------------------------
 
 // let characters = letters.map(list => list.split('').filter((letter, index) => !(index % 2))).join();
 // let result = characters.reduce((previous, next) => previous.concat(next)).join('');

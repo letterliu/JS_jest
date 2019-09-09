df17300695bd09dd877090564f62cd5fc15fe9e0
letterliu <letter0206@gmail.com>
2019-09-09 22:47:34 +0800

    mpdify jest/js_12.test.js

---
+++ jest/js_12.test.js
@@ -0,0 +1,81 @@
+
+
+console.info = function () { }; // 註解 => jest過濾方法
+const Matchers = require('../js_12');
+
+describe('質數倍數主程式：primeProgram', () => {
+
+  describe('主程式運作', () => {
+    it('回傳：{ "11": [11, 22, 33, 44, 55, 66, 77, 88, 99], "13": [13, 26, 39, 52, 65, 78, 91], "17": [17, 34, 51, 68, 85], "19": [19, 38, 57, 76, 95] }。', () => {
+      const termArray = [];
+      const result = Matchers.primeProgram(termArray);
+      expect(result).toEqual({ "11": [11, 22, 33, 44, 55, 66, 77, 88, 99], "13": [13, 26, 39, 52, 65, 78, 91], "17": [17, 34, 51, 68, 85], "19": [19, 38, 57, 76, 95] });
+    });
+    it('回傳：{ "7": [7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98] }。', () => {
+      const termArray = ['7'];
+      const result = Matchers.primeProgram(termArray);
+      expect(result).toEqual({ "7": [7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98] });
+    });
+  });
+
+  describe('空值與資料格式過濾', () => {
+    it('過濾：輸入值[a] => 格式錯誤，請重新輸入有效正整數。。', () => {
+      const invalidData = ['a'];
+      const result = () => Matchers.formatData(invalidData);
+      expect(result).toThrow(`輸入值[${invalidData}] => 格式錯誤，請重新輸入有效正整數。`);
+    });
+    it('過濾：輸入有效數值，字串轉成數值。', () => {
+      const validData = ['7'];
+      const result = Matchers.formatData(validData);
+      expect(result).toEqual([7]);
+    });
+  });
+
+  describe('替換新陣列(無輸入)', () => {
+    it('預設：[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]。', () => {
+      const array = [], termArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+      const result = Matchers.arrayLength(array, termArray);
+      expect(result).toEqual([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    });
+  });
+
+  describe('質數過濾器', () => {
+    it('成立：true。', () => {
+      const integer = 11;
+      const result = Matchers.isPrime(integer);
+      expect(result).toBeTruthy();
+    });
+  });
+
+  describe('質數組成新陣列', () => {
+    it('成立：[11, 19]。', () => {
+      const referenceArray = [11, 12, 15, 19];
+      const result = Matchers.primeArray(referenceArray);
+      expect(result).toEqual([11, 19]);
+    });
+  });
+
+  describe('倍數化物件', () => {
+    it('運轉：{"11": [11, 22, 33, 44, 55, 66, 77, 88, 99], "19": [19, 38, 57, 76, 95]}。', () => {
+      const selectArray = [11, 19];
+      const result = Matchers.multiplePrime(selectArray);
+      expect(result).toEqual({ "11": [11, 22, 33, 44, 55, 66, 77, 88, 99], "19": [19, 38, 57, 76, 95] });
+    });
+  });
+
+  describe('質數倍數化', () => {
+    it('運轉：[11, 22, 33, 44, 55, 66, 77, 88, 99]。', () => {
+      const selectArray = [], prime = 11, count = 1, maximum = 100;
+      const result = Matchers.isMultiple(selectArray, prime, count, maximum);
+      expect(result).toEqual([11, 22, 33, 44, 55, 66, 77, 88, 99]);
+    });
+  });
+
+  describe('陣列倍數後結果', () => {
+    it('打印：{"19": [19, 38, 57, 76, 95] }。', () => {
+      const termArray = { "19": [19, 38, 57, 76, 95] };
+      const result = Matchers.announced(termArray);
+      expect(result).toEqual({ "19": [19, 38, 57, 76, 95] });
+    });
+  });
+});
--- js_12.js
+++ js_12.js
@@ -2,6 +2,18 @@
 找出10~20的質數，並列出前述各質數在1~100的倍數
 */
 
+// Jest
+module.exports = {
+  primeProgram,
+  formatData,
+  arrayLength,
+  isPrime,
+  primeArray,
+  multiplePrime,
+  isMultiple,
+  announced
+};
+
 const result = primeProgram(process.argv.slice(2, 10));
 console.info(result);
 
@@ -21,70 +33,69 @@ function primeProgram(receivedata) {
   finally {
     console.info(`@(･ｪ･｡)@ 陣列質數遊戲：於 100 範圍內，質數倍數化。`);
   }
+}
 
-  // 判斷格式是否全整數
-  function formatData(data) {
-    const ELIMINATEDATA = /[^\d]|^0$/;
-    const invalidData = data.map(value => ELIMINATEDATA.test(value)).some(value => value);
-    const validNull = data.includes('');
-    if (invalidData || validNull) {
-      throw Error(`輸入值[${data}] => 格式錯誤，請重新輸入有效正整數。`);
-    }
-    return data.map(value => Number(value));
+// 判斷格式是否全整數
+function formatData(data) {
+  const ELIMINATEDATA = /[^\d]|^0$/;
+  const invalidData = data.map(value => ELIMINATEDATA.test(value)).some(value => value);
+  const validNull = data.includes('');
+  if (invalidData || validNull) {
+    throw Error(`輸入值[${data}] => 格式錯誤，請重新輸入有效正整數。`);
   }
+  return data.map(value => Number(value));
+}
 
-  // 判斷是否正確輸入數值，長度為零，替換預設新陣列
-  function arrayLength(array, termArray) {
-    if (!(array.length)) {
-      console.info(`未輸入任何資料，預設新陣列數值：[${termArray}]，運算中，請稍候。`);
-      return termArray;
-    }
-    console.info(`格式正確，新陣列數值：[${array}]，運算中，請稍候。`);
-    return array;
+// 判斷是否正確輸入數值，長度為零，替換預設新陣列
+function arrayLength(array, termArray) {
+  if (!(array.length)) {
+    console.info(`未輸入任何資料，預設新陣列數值：[${termArray}]，運算中，請稍候。`);
+    return termArray;
   }
+  console.info(`格式正確，新陣列數值：[${array}]，運算中，請稍候。`);
+  return array;
+}
 
-  // 質數過濾器
-  function isPrime(integer) {
-    for (let i = 2; i < integer; i++) {
-      if (!(integer % i)) return false;
-      return integer > 1;
-    }
+// 質數過濾器
+function isPrime(integer) {
+  for (let i = 2; i < integer; i++) {
+    if (!(integer % i)) return false;
   }
+  return integer > 1;
+}
 
-  // 將質數組成新陣列
-  function primeArray(referenceArray) {
-    return referenceArray.filter((integer) => {
-      if (isPrime(integer)) return integer;
-    });
-  }
+// 將質數組成新陣列
+function primeArray(referenceArray) {
+  return referenceArray.filter((integer) => {
+    if (isPrime(integer)) return integer;
+  });
+}
 
-  // 倍數化物件
-  function multiplePrime(selectPrime) {
-    return selectPrime.reduce((accumulator, prime) => {
-      const selectArray = [], count = 1;
-      accumulator[prime] = isMultiple(selectArray, prime, count);
-      return accumulator;
-    }, {});
-  }
+// 倍數化物件
+function multiplePrime(selectPrime) {
+  return selectPrime.reduce((accumulator, prime) => {
+    const selectArray = [], count = 1;
+    accumulator[prime] = isMultiple(selectArray, prime, count);
+    return accumulator;
+  }, {});
+}
 
-  // 質數倍數化
-  function isMultiple(selectArray, prime, count, maximum = 100) {
-    if (prime * count >= maximum) return selectArray;
-    selectArray.push(prime * count);
-    count++;
-    return isMultiple(selectArray, prime, count);
-  }
+// 質數倍數化
+function isMultiple(selectArray, prime, count, maximum = 100) {
+  if (prime * count >= maximum) return selectArray;
+  selectArray.push(prime * count);
+  count++;
+  return isMultiple(selectArray, prime, count);
+}
 
-  // 打印陣列倍數後結果
-  function announced(result) {
-    return new Map(Object.entries(result));
-    // result.forEach(element => console.info(element));
-    // Object.entries(result).forEach(([key, value]) => {
-    //   console.info(`質數：${key}，倍數${value}。`);
-    // });
-    // return result;
-    // });
-  }
+// 打印陣列倍數後結果
+function announced(result) {
+  // return new Map(Object.entries(result));
+  // result.forEach(element => console.info(element));
+  Object.entries(result).forEach(([key, value]) => {
+    console.info(`質數：${key}，倍數${value}。`);
+  });
+  return result;
 }
 
 //  // 質數倍數化
